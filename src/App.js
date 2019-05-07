b4a1388411dd1761c1ba92b113bc72b236409cf7
Parama92 <parama.kar@gmail.com>
2019-05-07 23:58:14 +0530

    initialized the react application

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Drumkit from "./components/Drumkit";
+import Clock from "./components/Clock"
+
+class App extends Component {
+  render() {
+    return <Clock />;
+  }
+}
+
+export default App;
