6ab6bffa3cb1cfb4fc7cb5cc41c98f2a34dccac1
Parama92 <parama.kar@gmail.com>
2019-05-08 23:41:59 +0530

    changed folder structure

---
+++ src/components/Drumkit/Drumkit.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./Drumkit.css";
+import DrumKey from "./DrumKey";
+import keys from "./keys";
+import boom from "./sounds/boom.wav";
+
+class Drumkit extends React.Component {
+  componentDidMount() {
+    window.addEventListener("keydown", function (e) {
+      const pressed = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+      if (!pressed) return;
+      pressed.play();
+    });
+  }
+
+  render() {
+    const keysComponent = keys.map(key => <DrumKey key={key.id} item={key} />);
+    return <div className="keys">{keysComponent}</div>;
+  }
+}
+
+export default Drumkit;
---
+++ src/components/Drumkit/keys.js
@@ -0,0 +1,58 @@
+const keys = [
+  {
+    id: 1,
+    kbd: "A",
+    code: 65,
+    sound: "clap"
+  },
+  {
+    id: 2,
+    kbd: "S",
+    code: 83,
+    sound: "hihat"
+  },
+  {
+    id: 3,
+    kbd: "D",
+    code: 68,
+    sound: "kick"
+  },
+  {
+    id: 4,
+    kbd: "F",
+    code: 70,
+    sound: "openhat"
+  },
+  {
+    id: 5,
+    kbd: "G",
+    code: 71,
+    sound: "boom"
+  },
+  {
+    id: 6,
+    kbd: "H",
+    code: 72,
+    sound: "ride"
+  },
+  {
+    id: 7,
+    kbd: "J",
+    code: 74,
+    sound: "snare"
+  },
+  {
+    id: 8,
+    kbd: "K",
+    code: 75,
+    sound: "tom"
+  },
+  {
+    id: 9,
+    kbd: "L",
+    code: 76,
+    sound: "tink"
+  }
+];
+
+export default keys;
