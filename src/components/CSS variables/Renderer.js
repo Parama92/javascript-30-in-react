d9c79e07e7cc69a8f94d7d74558f54db1a76cc42
Parama92 <parama.kar@gmail.com>
2019-05-09 22:27:55 +0530

    removed the data-sizing property

--- src/components/CSS variables/Renderer.js
+++ src/components/CSS variables/Renderer.js
@@ -8,13 +8,13 @@ class Renderer extends React.Component {
 
                 <div className="controls">
                     <label htmlFor="spacing">Spacing:</label>
-                    <input id="spacing" type="range" name="spacing" min="10" max="200" value={this.props.spacing} data-sizing="px" />
+                    <input id="spacing" type="range" name="spacing" min="10" max="200" value={this.props.spacing} onChange={this.props.onChange} />
 
                     <label htmlFor="blur">Blur:</label>
-                    <input id="blur" type="range" name="blur" min="0" max="25" value={this.props.blur} data-sizing="px" />
+                    <input id="blur" type="range" name="blur" min="0" max="25" value={this.props.blur} onChange={this.props.onChange} />
 
                     <label htmlFor="base">Base Color</label>
-                    <input id="base" type="color" name="base" value={this.props.base} />
+                    <input id="base" type="color" name="base" value={this.props.base} onChange={this.props.onChange} />
                 </div>
 
                 <img src="https://source.unsplash.com/7bwQXzbF6KE/800x500" />
