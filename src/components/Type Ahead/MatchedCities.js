605437f5553971d2fd2446a915fe1cfa834fbf88
Parama92 <parama.kar@gmail.com>
2019-05-18 22:48:41 +0530

    fixed the issue of <span class='hl'> not being applied properly

--- src/components/Type Ahead/MatchedCities.js
+++ src/components/Type Ahead/MatchedCities.js
@@ -1,22 +1,25 @@
 import React from 'react';
 
 class MatchedCities extends React.Component {
+    displayPlace = placeName => {
+        return placeName.map((chunk, i) => (
+            <React.Fragment key={placeName + chunk}>
+                {chunk}{i === placeName.length - 1 ? '' : <span className='hl'>{this.props.input}</span>}
+            </React.Fragment>
+        ));
+    }
     render() {
         const listComponent = this.props.list.length ?
             this.props.list.map(item => {
                 const exp = new RegExp(this.props.input, 'gi');
                 const cityName = [].concat(item.city.split(exp));
-                const stateName = [].concat(item.state.replace(exp));
+                const stateName = [].concat(item.state.split(exp));
                 return (<li key={`${item.city}, ${item.state}`}>
                     <span className='name'>
-                        {cityName[0]}
-                        <span className='hl'>{this.props.input}</span>
-                        {cityName[1] || ''},&nbsp;
-                        {stateName[0]}
-                        <span className='hl'>{this.props.input}</span>
-                        {stateName[1] || ''}
+                        {this.displayPlace(cityName)},&nbsp;
+                        {this.displayPlace(stateName)}
                     </span>
-                    <span className='population'>{item.population}</span>
+                    <span className='population'>{`${item.population.toLocaleString()}`}</span>
                 </li>)
             })
             :
